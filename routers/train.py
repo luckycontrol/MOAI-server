3c9b4d0c1a463c81601a6244796328ceb4d27625
luckycontrol <beansu1@naver.com>
2024-12-23 23:26:36 +0900

    background training 추가

--- routers/train.py
+++ routers/train.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, BackgroundTasks
 from typing import Dict
 from models.train import TrainRequest
 
@@ -7,12 +7,13 @@ from containers.yolo_container import train_yolo, inference_yolo
 router = APIRouter()
 
 @router.post("/train")
-async def train(request: TrainRequest) -> Dict:
+async def train(request: TrainRequest, background_tasks: BackgroundTasks) -> Dict:
     """
     학습 시작 엔드포인트.
 
     Args:
         request (TrainRequest): 학습에 필요한 정보
+        background_tasks (BackgroundTasks): 백그라운드 작업 관리자
 
     Returns:
         Dict: 요청 처리 결과
@@ -20,11 +21,11 @@ async def train(request: TrainRequest) -> Dict:
 
     try:
         if request.train_params.model_type == "yolo":
-            train_yolo(request)
+            background_tasks.add_task(train_yolo, request)
 
         return {
             "status": "success",
-            "message": "학습 시작",
+            "message": "학습 진행 중",
             "data": request.model_dump()
         }
     
