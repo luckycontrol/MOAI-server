5410d6df205406add80ebc6ffa36c8deccdce46c
luckycontrol <beansu1@naver.com>
2025-02-04 13:39:01 +0900

    pt 이동 에러

--- routers/stop.py
+++ routers/stop.py
@@ -6,6 +6,7 @@ import logging
 import os
 import shutil
 import threading
+from distutils.dir_util import copy_tree
 
 thread_lock = threading.Lock()
 
@@ -50,7 +51,7 @@ async def stop(stop_params: StopParams) -> Dict:
             weights_path = os.path.join(version_path, "training_result", "weights")
             if os.path.exists(version_path) and os.path.exists(weights_path) and any(f.endswith('.pt') for f in os.listdir(weights_path)):
                 with thread_lock:
-                    shutil.copy(weights_path, version_path)
+                    copy_tree(weights_path, version_path)
                     # shutil.move(weights_path, version_path)
             return {
                 "status": "success",
